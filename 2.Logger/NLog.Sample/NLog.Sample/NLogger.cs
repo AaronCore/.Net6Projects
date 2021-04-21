9318031daeceb8f3250adefada401d3e6fe7552d
AaronCore <969340992@qq.com>
2021-04-21 09:32:22 +0800

    edit log

--- 2.Logger/NLog.Sample/NLog.Sample/NLogger.cs
+++ 2.Logger/NLog.Sample/NLog.Sample/NLogger.cs
@@ -12,60 +12,108 @@ namespace NLog.Sample
         /// 非常详细的日志
         /// </summary>
         /// <param name="message"></param>
-        /// <param name="exception"></param>
-        public static void Trace(string message, Exception exception = null)
+        public static void Trace(string message)
         {
             Logger.Trace(message);
+        }
+
+        /// <summary>
+        /// 非常详细的日志
+        /// </summary>
+        /// <param name="exception"></param>
+        public static void Trace(Exception exception)
+        {
+            Logger.Trace(exception);
         }
 
         /// <summary>
         /// 调试信息
         /// </summary>
         /// <param name="message"></param>
-        /// <param name="exception"></param>
-        public static void Debug(string message, Exception exception = null)
+        public static void Debug(string message)
         {
             Logger.Debug(message);
+        }
+
+        /// <summary>
+        /// 调试信息
+        /// </summary>
+        /// <param name="exception"></param>
+        public static void Debug(Exception exception)
+        {
+            Logger.Debug(exception);
         }
 
         /// <summary>
         /// 信息消息
         /// </summary>
         /// <param name="message"></param>
-        /// <param name="exception"></param>
-        public static void Info(string message, Exception exception = null)
+        public static void Info(string message)
         {
             Logger.Info(message);
+        }
+
+        /// <summary>
+        /// 信息消息
+        /// </summary>
+        /// <param name="exception"></param>
+        public static void Info(Exception exception)
+        {
+            Logger.Info(exception);
         }
 
         /// <summary>
         /// 警告信息
         /// </summary>
         /// <param name="message"></param>
-        /// <param name="exception"></param>
-        public static void Warn(string message, Exception exception = null)
+        public static void Warn(string message)
         {
             Logger.Warn(message);
+        }
+
+        /// <summary>
+        /// 警告信息
+        /// </summary>
+        /// <param name="exception"></param>
+        public static void Warn(Exception exception)
+        {
+            Logger.Warn(exception);
         }
 
         /// <summary>
         /// 错误信息
         /// </summary>
         /// <param name="message"></param>
-        /// <param name="exception"></param>
-        public static void Error(string message, Exception exception = null)
+        public static void Error(string message)
         {
             Logger.Error(message);
+        }
+
+        /// <summary>
+        /// 错误信息
+        /// </summary>
+        /// <param name="exception"></param>
+        public static void Error(Exception exception)
+        {
+            Logger.Error(exception);
         }
 
         /// <summary>
         /// 非常严重的错误
         /// </summary>
         /// <param name="message"></param>
-        /// <param name="exception"></param>
-        public static void Fatal(string message, Exception exception = null)
+        public static void Fatal(string message)
         {
             Logger.Fatal(message);
+        }
+
+        /// <summary>
+        /// 非常严重的错误
+        /// </summary>
+        /// <param name="exception"></param>
+        public static void Fatal(Exception exception)
+        {
+            Logger.Fatal(exception);
         }
 
         /// <summary>
@@ -81,12 +129,12 @@ namespace NLog.Sample
         private static void WriteLog(object message, string folder)
         {
             var dt = DateTime.Now;
-            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            // var baseDir = AppDomain.CurrentDomain.BaseDirectory;
             lock (LogWriteLockObj)
             {
                 try
                 {
-                    var dir = $@"{baseDir}\Logs\{dt:yyyy-MM-dd}\{folder}";
+                    var dir = $@"{Environment.CurrentDirectory}\NLogs\{dt:yyyy-MM-dd}\{folder}";
                     if (!Directory.Exists(dir))
                     {
                         Directory.CreateDirectory(dir);
@@ -95,7 +143,7 @@ namespace NLog.Sample
                     var path = Path.Combine(dir + @"\" + dt.ToString("yyyy-MM-dd HH") + ".log");
                     using (var sw = new StreamWriter(path, true))
                     {
-                        sw.WriteLine("时间：{0} | 内容：{1} \n", dt.ToString("yyyy-MM-dd HH:mm:ss"), message);
+                        sw.WriteLine("{0} | {1}", dt.ToString("yyyy-MM-dd HH:mm:ss"), message);
                         sw.Close();
                     }
                 }
