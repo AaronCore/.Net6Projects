1ae9081bc5f03d6a4843e4d906605790b8c8d36b
AaronCore <969340992@qq.com>
2021-05-13 09:39:00 +0800

    add snowflake

---
+++ 9.Tools/Snowflake.Sample/Snowflake.Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Snowflake.Core;
+
+namespace Snowflake.Sample
+{
+    class Program
+    {
+        // 布式全局唯一ID算法
+        static void Main(string[] args)
+        {
+            var worker = new IdWorker(1, 1);
+            long id = worker.NextId();
+            Console.WriteLine(id);
+        }
+    }
+}
