539765a4e3b348d72322c6f44af3f4f64508f45b
AaronCore <969340992@qq.com>
2021-05-13 09:24:42 +0800

    add email

---
+++ 8.Common/Common/Common.Library/Email/EmailHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace Common.Library.Email
+{
+    /// <summary>
+    /// C#邮件发送类
+    /// </summary>
+    public class EmailHelper
+    {
+        public bool SendMail(EmailSender sender, out string errorMsg)
+        {
+            //声明一个Mail对象
+            MailMessage mymail = new MailMessage();
+            //发件人地址
+            //如是自己，在此输入自己的邮箱
+            mymail.From = new MailAddress(sender.SendAddress, sender.SendNickName, Encoding.UTF8);
+            //收件人地址
+            mymail.To.Add(new MailAddress(sender.ReceiveAddress));
+            //邮件主题
+            mymail.Subject = sender.Title;
+            //邮件标题编码
+            mymail.SubjectEncoding = Encoding.UTF8;
+            //发送邮件的内容
+            mymail.Body = sender.Content;
+            //邮件内容编码
+            mymail.BodyEncoding = Encoding.UTF8;
+            //添加附件
+            foreach (var attachment in sender.Attachments)
+            {
+                mymail.Attachments.Add(attachment);
+            }
+
+            //抄送到其他邮箱
+            foreach (var str in sender.Cc)
+            {
+                mymail.CC.Add(new MailAddress(str));
+            }
+
+            //是否是HTML邮件
+            mymail.IsBodyHtml = sender.IsBodyHtml;
+            //邮件优先级
+            mymail.Priority = sender.MailPriority;
+            //创建一个邮件服务器类
+            SmtpClient myclient = new SmtpClient();
+            myclient.Host = sender.Host;
+            //SMTP服务端口
+            myclient.Port = sender.Port;
+            //验证登录
+            myclient.Credentials = new NetworkCredential(sender.Username, sender.Password);//"@"输入有效的邮件名, "*"输入有效的密码
+            try
+            {
+                myclient.Send(mymail);
+                errorMsg = "";
+                return true;
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.Message;
+                return false;
+            }
+
+        }
+    }
+}
---
+++ 8.Common/Common/Common.Library/Email/EmailSender.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections.Generic;
+using System.Net.Mail;
+
+namespace Common.Library.Email
+{
+    public class EmailSender
+    {
+        private string sendAddress;
+        private string sendNickName;
+        private string receiveAddress;
+        private string receiveNickName;
+        private string title;
+        private string content;
+        private List<Attachment> attachments;
+        private List<string> cc;
+        private bool isBodyHtml;
+        private MailPriority mailPriority;
+        private string host;
+        private int port;
+        private string username;
+        private string password;
+
+        public EmailSender()
+        {
+            attachments = new List<Attachment>();
+            cc = new List<string>();
+        }
+
+        /// <summary>
+        /// 发送人地址
+        /// </summary>
+        public string SendAddress
+        {
+            get { return sendAddress; }
+            set { sendAddress = value; }
+        }
+
+        /// <summary>
+        /// 发送人昵称
+        /// </summary>
+        public string SendNickName
+        {
+            get { return sendNickName; }
+            set { sendNickName = value; }
+        }
+
+        /// <summary>
+        /// 接收人地址
+        /// </summary>
+        public string ReceiveAddress
+        {
+            get { return receiveAddress; }
+            set { receiveAddress = value; }
+        }
+
+        /// <summary>
+        /// 接收人昵称
+        /// </summary>
+        public string ReceiveNickName
+        {
+            get { return receiveNickName; }
+            set { receiveNickName = value; }
+        }
+
+        /// <summary>
+        /// 邮件标题
+        /// </summary>
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        /// <summary>
+        /// 邮件内容
+        /// </summary>
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        /// <summary>
+        /// 附件列表
+        /// </summary>
+        public List<Attachment> Attachments
+        {
+            get { return attachments; }
+            set { attachments = value; }
+        }
+
+        /// <summary>
+        /// 抄送列表
+        /// </summary>
+        public List<string> Cc
+        {
+            get { return cc; }
+            set { cc = value; }
+        }
+
+        /// <summary>
+        /// 是否为html邮件
+        /// </summary>
+        public bool IsBodyHtml
+        {
+            get { return isBodyHtml; }
+            set { isBodyHtml = value; }
+        }
+
+        /// <summary>
+        /// 邮件优先级
+        /// </summary>
+        public MailPriority MailPriority
+        {
+            get { return mailPriority; }
+            set { mailPriority = value; }
+        }
+
+        /// <summary>
+        /// smtp服务器
+        /// </summary>
+        public string Host
+        {
+            get { return host; }
+            set { host = value; }
+        }
+
+        /// <summary>
+        /// 端口号
+        /// </summary>
+        public int Port
+        {
+            get { return port; }
+            set { port = value; }
+        }
+
+        /// <summary>
+        /// 邮件服务器验证用户名
+        /// </summary>
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        /// <summary>
+        /// 邮件服务器验证密码
+        /// </summary>
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
