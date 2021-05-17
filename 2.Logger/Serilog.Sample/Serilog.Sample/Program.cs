33b12bb66d897eaf7ce89c72b599e886dad0b72f
AaronCore <969340992@qq.com>
2021-05-17 14:46:09 +0800

    add Serilog

---
+++ 2.Logger/Serilog.Sample/Serilog.Sample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Serilog.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("Hello, world!");
+
+            int a = 10, b = 0;
+            try
+            {
+                Log.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Something went wrong");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+    }
+}
