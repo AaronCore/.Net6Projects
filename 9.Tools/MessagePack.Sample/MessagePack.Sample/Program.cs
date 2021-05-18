3f3e9ed74ffd3c7a5c32a3c9b3d0eaafb4235343
AaronCore <969340992@qq.com>
2021-05-18 11:23:48 +0800

    add MessagePack.Sample

---
+++ 9.Tools/MessagePack.Sample/MessagePack.Sample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MessagePack.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var peopleInfo = new People
+            {
+                Age = 25,
+                FirstName = "xiaoxiao",
+                LastName = "wu",
+            };
+            byte[] bytes = MessagePackSerializer.Serialize(peopleInfo);
+            var people = MessagePackSerializer.Deserialize<People>(bytes);
+
+            var json = MessagePackSerializer.ConvertToJson(bytes);
+            Console.WriteLine(people);
+        }
+    }
+    [MessagePackObject]
+    public class People
+    {
+        [Key(0)]
+        public int Age { get; set; }
+
+        [Key(1)]
+        public string FirstName { get; set; }
+
+        [Key(2)]
+        public string LastName { get; set; }
+
+        [IgnoreMember]
+        public string FullName => FirstName + LastName;
+    }
+}
