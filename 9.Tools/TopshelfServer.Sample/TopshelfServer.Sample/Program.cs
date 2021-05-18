9419010a33459751edfe389c1b10ba1932fb992e
AaronCore <969340992@qq.com>
2021-05-18 10:55:36 +0800

    add Topshelf

---
+++ 9.Tools/TopshelfServer.Sample/TopshelfServer.Sample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Topshelf;
+
+namespace TopshelfServer.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+                {
+                    x.Service<LoggingService>();
+                    x.EnableServiceRecovery(r => r.RestartService(TimeSpan.FromSeconds(10)));
+                    x.SetServiceName("TopshelfTestService");
+                    x.StartAutomatically();
+                }
+            );
+        }
+    }
+    public class LoggingService : ServiceControl
+    {
+        private void Log(string logMessage)
+        {
+            Console.WriteLine(logMessage);
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            Log("Starting");
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            Log("Stopping");
+            return true;
+        }
+    }
+}
